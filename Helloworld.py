7cc6cf066a9d1a30c3cad5e9c934eeccb88a3723
Aeriel Kawasaki <aerielellisk5@gmail.com>
2022-10-26 12:37:13 +0000

    forgot to push up changes from yesterday

--- Helloworld.py
+++ Helloworld.py
@@ -1,1 +1,11 @@
 print("Hello World")
+
+#This is a full line comment
+#Need to comment each line individually
+
+print("Hello, World") # this is a trailing comment
+
+
+my_str = "This is a simple string"
+
+print(my_str)
