6888329089466985479864d9315c62777b09c92c
Aeriel Kawasaki <aerielellisk5@gmail.com>
2022-10-30 05:19:08 +0000

    confused as to why monday's problem is wrong, ask for help

--- hackerrank.py
+++ hackerrank.py
@@ -1,8 +1,24 @@
-def solve(s):
+# def solve(s):
     
-    new = str(s)
-    next = new.split()
-    return(new)
+#     new = str(s)
+#     next = new.split()
+#     return(new)
     
-print(solve("hello there"))
+# print(solve("hello there"))
+    
+    
+n = 24
+if n % 2 == 0:
+    if 2 <= n <= 5 or n >= 20:
+        print (n > 20)
+        print("Not Weird")
+    else:
+        pass
+    if 6 <= n <= 20:
+        print("Weird")
+else: 
+    print("Weird")    
+    
+
+    
     
