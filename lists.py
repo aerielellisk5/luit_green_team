3c7c1eeae1b4645df03c49a879fc9268bc4c3a39
Aeriel Kawasaki <aerielellisk5@gmail.com>
2022-11-01 23:25:58 +0000

    lists

---
+++ lists.py
@@ -0,0 +1,32 @@
+# var_list = []
+
+# # print(type(var_list))
+
+# var_list.append(493)
+# var_list.append('009')
+# var_list.append(151)
+
+# print(var_list)
+
+# print(var_list[2])
+# print(var_list[-1])
+
+# print(len(var_list))
+
+# print(dir(var_list))
+# super helpful to see what methods/functions you have access to in the data type
+
+number= [0,1,2,3,4,4,4,4,49]
+
+numbers = list(set(number))
+print(numbers)
+
+# print(numbers)
+
+# print(list(range(0,15)))
+
+
+# numbers= [0,1,2,3,4,5,6,7,8,9]
+
+# for number in numbers:
+#     print(number)
